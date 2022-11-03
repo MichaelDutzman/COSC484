009d1d9e28c9fdcccab706ed2bf2d9c34e17bfee
MANGEL9-development <99140478+MANGEL9-development@users.noreply.github.com>
2022-11-02 22:25:37 -0400

    Addition of login page, signup page.
    I added a function called loadSite() in script-main.js . For this function to work properly, it should be called as the event handler for when the body loads. In other words, the body should be written with <body onload="loadSite(x)">. More information is written in the documentation, but the number that is passed to the loadSite function should indicate how many folders to go up to reach the folder that has the index.html file. So the homepage (index.html) should call this function with level set to 0 (because index.html is in the same folder as itself).
    
    The links don't really work yet, so that last paragraph isn't really that important for now.

--- Stylish Donuts/JS/common_snippets.js
+++ Stylish Donuts/JS/common_snippets.js
@@ -4,18 +4,108 @@
 */
 
 function addSnippets(level){
-    addFooter();
-    addNavBar(level);
+    /* For now, snippets are coded as strings that are included into webpages
+        by changing their respective elements' innerHTML. The goal is to have
+        snippets in their own html files. Those files would be read and imported
+        into the webpages. That should make them easier to work with.
+     */
+    var snippets=[addFooter,addNavBar];
+    snippets.forEach((snippet)=>{
+        try{
+            snippet(level);
+        }
+        catch(err){
+            //do nothing
+        }
+    });
 }
 
-function addFooter(){
+function addFooter(level){
     var footer=document.getElementsByTagName("footer")[0];
-    var footerHTML=""; // this will be filled after a footer idea is solidified
+    var footerHTML=
+        "<div class=\"footer-section\" id=\"footer-sitename\">" +
+        "    <span class=\"website-title\" id=\"footer-website-title\">Stylish Donuts</span>" +
+        "</div>" +
+        "<div class=\"footer-section\" id=\"footer-section-a\">" +
+        "    <h2 class=\"footer-section-title\">Order<!-- sample text --></h2>" +
+        "    <ul>" +
+        "        <li><a href=\"#\">Vegan<!-- sample text --></a></li>" +
+        "        <li><a href=\"#\">Peanut Allergy<!-- sample text --></a></li>" +
+        "        <li><a href=\"#\">Dairy Free<!-- sample text --></a></li>" +
+        "        <li><a href=\"#\">Gluten Free<!-- sample text --></a></li>" +
+        "        <li><a href=\"#\">Keto<!-- sample text --></a></li>" +
+        "    </ul>" +
+        "</div>" +
+        "<div class=\"footer-section\" id=\"footer-section-b\">" +
+        "    <h2 class=\"footer-section-title\">Section B<!-- sample text --></h2>" +
+        "    <ul>" +
+        "        <li><a href=\"#\">That<!-- sample text --></a></li>" +
+        "        <li><a href=\"#\">Could<!-- sample text --></a></li>" +
+        "        <li><a href=\"#\">Be<!-- sample text --></a></li>" +
+        "        <li><a href=\"#\">Included<!-- sample text --></a></li>" +
+        "        <li><a href=\"#\">At The<!-- sample text --></a></li>" +
+        "        <li><a href=\"#\">Bottom<!-- sample text --></a></li>" +
+        "        <li><a href=\"#\">Of The<!-- sample text --></a></li>" +
+        "    </ul>" +
+        "</div>" +
+        "<div class=\"footer-section\" id=\"footer-section-c\">" +
+        "    <h2 class=\"footer-section-title\">Section C</h2>" +
+        "    <ul>" +
+        "        <li><a href=\"#\">Webpages</a></li>" +
+        "        <li><a href=\"#\">Such</a></li>" +
+        "        <li><a href=\"#\">As a</a></li>" +
+        "        <li><a href=\"#\">Sitemap</a></li>" +
+        "        <li><a href=\"#\">Or Socials</a></li>" +
+        "    </ul>" +
+        "</div>" +
+        "<div id=\"footer-copyright\">" +
+        "    <span id=\"copyright-statement\">COSC484 Group 4 &copy; 2022</span>" +
+        "    <!-- Maybe make \"Group 4\" into a link to our github or something -->" +
+        "</div>";
 
 
     footer.innerHTML=footerHTML;
 }
 
 function addNavBar(level){
+    const navElement=document.getElementById("navigation-container");
+    //getSnippetFromFile("nav-bar.html",navElement,level);
+    var navBarHTML=
+        "<div id=\"navigation-bar\">" +
+        "    <ul id=\"nav-links\">" +
+        "        <li><a href=\"#\" class=\"homepage-link\"></a></li><!--" +
+        "     --><li><a href=\"#\">Order<!-- sample text --></a></li><!--" +
+        "     --><li><a href=\"#\">Account<!-- sample text --></a></li><!--" +
+        "     --><li class=\"current-page\"><a href=\"#\">Cart<!-- sample text --></a></li><!--" +
+        "     --><li><a href=\"#\">About<!-- sample text --></a></li>" +
+        "    </ul>" +
+        "</div>" +
+        "<div id=\"search-container\">" +
+        "    <form id=\"searchbar\">" +
+        "        <div id=\"search-input-container\">" +
+        "            <input type=\"search\" name=\"searchquery\" id=\"search-input\" placeholder=\"Search for donuts...\" />" +
+        "            <button type=\"submit\">" +
+        "                <div id=\"search-button-onhover\"></div>" +
+        "            </button>" +
+        "        </div>" +
+        "    </form>" +
+        "</div>";
+    
+    navElement.innerHTML=navBarHTML;
+}
 
+function getSnippetFromFile(filename,element,level){
+    var xhttp=new XMLHttpRequest();
+    xhttp.onload=function(){
+        let snippet=this.response;
+        snippet=editLinks(snippet,level);
+        document.getElementById(id).innerHTML=snippet;
+    }
+    xhttp.open("GET","Snippets/"+filename);
+    xhttp.send();
+}
+
+function editLinks(str,level){
+    var dots="../".repeat(level);
+    return str.replaceAll("href=\"","href=\""+dots);
 }
--- Stylish Donuts/JS/script-main.js
+++ Stylish Donuts/JS/script-main.js
@@ -1,6 +1,21 @@
+/**
+ * "Parallax scrolling is a technique in computer graphics where background images move past
+ * the camera more slowly than foreground images, creating an illusion of depth in a 2D scene
+ * of distance." - Wikipedia.
+ * 
+ *   This funcition applies this effect on the the passed object by attaching an event handler
+ * to the window's scroll event. When the user scrolls, the passed object's background is moved,
+ * but only by a fraction of the distance scrolled by the user.
+ *   Note: This effect is set to not work on mobile devices because of problems caused by
+ * asynchronous scrolling.
+ * 
+ * @param {HTMLElement} obj - the HTML element whose background will be manipulated to achieve
+ *                            a parallax scrolling effect
+ */
 function parallaxScroll(obj){
     var isMobile=(/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) || window.innerWidth<=1024);
     if(!isMobile){
+        obj.style.backgroundAttachment="fixed";
         document.addEventListener("scroll",function(e){
             obj.style.backgroundPositionY=(50+(window.scrollY/25))+"%";
         });
@@ -12,10 +27,15 @@ function parallaxScroll(obj){
  * @param {int} level the level of the current page. 0 is for the homepage, 1 is for a subsite, 2 for a subsubsite, etc.
  */
 function loadSite(level){
-    //addSnippets(level);
+    addSnippets(level);
     enableSearchBarFocusEffect("search-input","searchbar");
 }
 
+/**
+ * The 
+ * @param {string} inputID 
+ * @param {string} parentID 
+ */
 function enableSearchBarFocusEffect(inputID,parentID){
     var input=document.getElementById(inputID);
     input.addEventListener("focus",function(e){
@@ -25,9 +45,39 @@ function enableSearchBarFocusEffect(inputID,parentID){
         blurParent(parentID);
     })
 }
+/**
+ * Focuses an element's parent by adding a "focus" class to the parent
+ * @param {string} id the element's parent's ID
+ */
 function focusParent(id){
     document.getElementById(id).classList.add("focused");
 }
-function blurParent(id){
+/**
+ * Blurs an element's parent by removing the "focus" class from the parent
+ * @param {string} id the element's parent's ID
+ */
+ function blurParent(id){
     document.getElementById(id).classList.remove("focused");
+}
+
+/**
+ * Changes an input's value according to whether the + or - button was
+ *      pressed. This function should be set as the onclick handler for
+ *      both the increase and decrease buttons.
+ * @param {string} targetID the ID of the input whose value will change
+ * @param {int} increment the amount that the input's value will be
+ *      incremented. It is recomended that this be set to 1 for the increase
+ *      button and -1 for the decrease button.
+ * @returns false. This is so that the form isn't submitted when the user
+ *      tries to change an input's value.
+ */
+function quantityChooser(targetID,increment){
+    const targetElement=document.getElementById(targetID);
+    var currentValue=parseInt(targetElement.value);
+    const min=(targetElement.min || 0);
+    const max=(targetElement.max || (currentValue+increment));
+    targetElement.value=Math.min(
+            Math.max((currentValue+increment),min),
+        max);
+    return false;
 }
